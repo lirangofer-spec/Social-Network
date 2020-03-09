dd3b08b4abdb3d304d3fd9f4c14d2265be2dea73
lirangofer-spec <lirangofer@gmail.com>
2020-03-09 08:36:54 +0200

    initial commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -65,7 +65,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get('jwtToken'),
+        config.get('jwtSecret'),
         { expireIn: 360000 },
         (err, token) => {
           if (err) throw err;
